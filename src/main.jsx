d8e4261a768c42137c57ffd35042c13cf44c3162
jasontran1403 <hainguyentran14@gmail.com>
2026-01-29 15:40:52 +0700

    update fix bug toast container

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,7 @@ import "slick-carousel/slick/slick.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './assets/main.css';
-import ToastContainer from './components/Toast/ToastContainer'; // Component mới
 
 createRoot(document.getElementById('root')).render(
-  <ToastContainer> {/* ToastContainer bao quanh toàn bộ app */}
-    <RouterProvider router={router} />
-  </ToastContainer>
+  <RouterProvider router={router} />
 );
