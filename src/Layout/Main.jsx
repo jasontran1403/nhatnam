1a7086210e4c928a3c588e67c4c4bc23420ce74d
jasontran1403 <hainguyentran14@gmail.com>
2026-01-30 22:43:34 +0700

    update fix route not reset position page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,15 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import Footer1 from '../Components/Footer/Footer1';
 import Header3 from '../Components/Header/Header3';
+import ScrollToTop from './ScrollToTop';
 
 const Main = () => {
-    return (
-        <div className='main-page-area bg-color2'>
-            <Header3></Header3>
-            <Outlet></Outlet>
-            <Footer1></Footer1>
-        </div>
-    );
+  return (
+    <div className='main-page-area bg-color2'>
+      <Header3></Header3>
+      
+      <ScrollToTop />
+      
+      <Outlet></Outlet>
+      <Footer1></Footer1>
+    </div>
+  );
 };
 
 export default Main;
